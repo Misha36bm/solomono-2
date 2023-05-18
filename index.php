4caa16abbd3c23549d10ffc5c98589420e13e916
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-18 04:29:55 +0300

    add function for push dump to DB

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
 <?php
+push_dump();
 
+
+
+function push_dump()
+{
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '123';
+    $database = 'tz_2';
+
+    try {
+        $pdo = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+
+    $sql = file_get_contents(__DIR__ . '/dump.sql');
+
+    $statements = explode(';', $sql);
+
+    foreach ($statements as $statement) {
+        if (!empty(trim($statement))) {
+            $pdo->exec($statement);
+        }
+    }
+
+    $pdo = null;
+}
